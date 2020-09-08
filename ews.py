ed9da6b4ec150435951f42e613f02ec0129bdbda
markus <markus@norad.de>
2020-09-08 10:56:49 +0200

    Some cleaning and fixing (conpot)

--- ews.py
+++ ews.py
@@ -1121,65 +1121,67 @@ def emobility():
 
 
 def conpot():
+
     MODUL  = "CONPOT"
-    logme(MODUL,"Starting Conpot Modul.",("P1"),ECFG)
+    logme(MODUL, "Starting Conpot Modul.", ("P1"), ECFG)
 
-    # collect honeypot config dic
+    """ collect honeypot config dic """
 
-    ITEMS  = ("conpot","nodeid","logfile")
-    HONEYPOT = readcfg(MODUL,ITEMS,ECFG["cfgfile"])
+    ITEMS  = ("conpot", "nodeid", "logfile")
+    HONEYPOT = readcfg(MODUL, ITEMS, ECFG["cfgfile"])
 
-    # if multiple logfiles:
-    conpotlog=glob.glob(HONEYPOT["logfile"])
+    """ if multiple logfiles """
+    conpotlog = glob.glob(HONEYPOT["logfile"])
     if len(conpotlog) < 1:
         logme(MODUL, "[ERROR] Missing of incorrect LogFile for conpot. Skip !", ("P3", "LOG"), ECFG)
 
-    index=0
+    index = 0
     orgModul = MODUL
 
     for logfile in conpotlog:
-        index+=1
+        index += 1
         if len(conpotlog) > 1:
-            MODUL=orgModul + "-" + str(index)
+            MODUL = orgModul + "-" + str(index)
 
-        # logfile file exists ? should not be triggered when using glob.glob.
+        """ logfile file exists ? should not be triggered when using glob.glob. """
         if os.path.isfile(logfile) is False:
-            logme(MODUL,"[ERROR] Missing LogFile " + logfile + ". Skip !",("P3","LOG"),ECFG)
+            logme(MODUL, "[ERROR] Missing LogFile " + logfile + ". Skip !", ("P3", "LOG"), ECFG)
 
         # count limit
-        imin = int(countme(MODUL,'fileline',-1,ECFG))
+        imin = int(countme(MODUL, 'fileline', -1, ECFG))
 
         if int(ECFG["sendlimit"]) > 0:
-            logme(MODUL,"Send Limit is set to : " + str(ECFG["sendlimit"]) + ". Adapting to limit!",("P1"),ECFG)
+            logme(MODUL, "Send Limit is set to : " + str(ECFG["sendlimit"]) + ". Adapting to limit!", ("P1"), ECFG)
 
         I = 0 ; x = 0 ; y = 1 ; J = 0
 
-        esm = ewsauth(ECFG["username"],ECFG["token"])
+        esm = ewsauth(ECFG["username"], ECFG["token"])
         jesm = ""
 
         while True:
 
-            x,y = viewcounter(MODUL,x,y)
+            x, y = viewcounter(MODUL, x, y)
 
             I += 1
 
             if int(ECFG["sendlimit"]) > 0 and I > int(ECFG["sendlimit"]):
                 break
 
-            line = getline(logfile,(imin + I)).rstrip()
-            currentline=imin+I
+            line = getline(logfile, (imin + I)).rstrip()
+            currentline = imin + I
 
             if len(line) == 0:
                 break
             else:
-                # parse json
+                """ parse json """
                 try:
                     content = json.loads(line)
+
                 except ValueError as e:
-                    logme(MODUL,"Invalid json entry found in line "+str(currentline)+", skipping entry.",("P3"),ECFG)
-                    countme(MODUL,'fileline',-2,ECFG)
-                    J+=1
-                    pass # invalid json
+                    logme(MODUL, "Invalid json entry found in line " + str(currentline) + ", skipping entry.", ("P3"), ECFG)
+                    countme(MODUL, 'fileline', -2, ECFG)
+                    J += 1
+
                 else:
                     DATA =    {
                                 "aid"       : HONEYPOT["nodeid"],
@@ -1192,14 +1194,14 @@ def conpot():
                                 "tadr"      : "%s" % content['dst_ip'],
                                 "tprot"     : "tcp",
                                 "tport"     : "-1",
-                            }
+                              }
 
                     REQUEST = {
                                 "description" : "Conpot Honeypot",
-                            }
+                              }
 
 
-                    # Collect additional Data
+                    """ Collect additional Data """
 
                     ADATA =   {
                                 "conpot_event_type"    :   "%s" % content['event_type'],
@@ -1211,21 +1213,20 @@ def conpot():
                                 "hostname": ECFG["hostname"],
                                 "externalIP": externalIP,
                                 "internalIP": internalIP
+                              }
 
-                            }
+                    """ generate template and send """
 
-                    # generate template and send
+                    esm = buildews(esm, DATA, REQUEST, ADATA)
+                    jesm = buildjson(jesm, DATA, REQUEST, ADATA)
 
-                    esm = buildews(esm,DATA,REQUEST,ADATA)
-                    jesm = buildjson(jesm,DATA,REQUEST,ADATA)
-
-                    countme(MODUL,'fileline',-2,ECFG)
-                    countme(MODUL,'daycounter', -2,ECFG)
+                    countme(MODUL, 'fileline', -2, ECFG)
+                    countme(MODUL, 'daycounter', -2, ECFG)
 
                     if ECFG["a.verbose"] is True:
-                        verbosemode(MODUL,DATA,REQUEST,ADATA)
+                        verbosemode(MODUL, DATA, REQUEST, ADATA)
 
-        # Cleaning linecache
+        """ Cleaning linecache """
         clearcache()
 
         if int(esm.xpath('count(//Alert)')) > 0:
@@ -1234,7 +1235,7 @@ def conpot():
         writejson(jesm)
 
         if y  > 1:
-            logme(MODUL,"%s EWS alert records send ..." % (x+y-2-J),("P2"),ECFG)
+            logme(MODUL, "%s EWS alert records send ..." % (x + y - 2 - J), ("P2"), ECFG)
     return
 
 
