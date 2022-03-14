003321a653c9a6ee068de0510fbdee9ff8945aff
markus <markus@norad.de>
2022-03-14 13:38:13 +0100

    PortSentry Support

--- ews.py
+++ ews.py
@@ -1243,6 +1243,7 @@ def citrix():
     citrix.finAlert()
     return()
 
+
 def redishoneypot():
 
     redishoneypot = EAlert('redishoneypot', ECFG)
@@ -1336,6 +1337,51 @@ def endlessh():
     endlessh.finAlert()
     return()    
 
+
+def sentrypeer():
+
+   sentrypeer = EAlert('sentrypeer', ECFG)
+
+    ITEMS = ['sentrypeer', 'nodeid', 'logfile']
+    HONEYPOT = (sentrypeer.readCFG(ITEMS, ECFG['cfgfile']))
+
+    if 'error_files' in HONEYPOT and HONEYPOT['error_files'] is False:
+        print(f"    -> {HONEYPOT['error_files_msg']}. Skip Honeypot.")
+        return()
+
+    while True:
+        line = sentrypeer.lineREAD(HONEYPOT['logfile'], 'json')
+
+        if len(line) == 0:
+            break
+        if line == 'jsonfail':
+            continue
+
+        sentrypeer.data('analyzer_id', HONEYPOT['nodeid']) if 'nodeid' in HONEYPOT else None
+
+        if 'event_timestamp' in line:
+            sentrypeer.data('timestamp', f"{line['event_timestamp'][0:19]}")
+            sentrypeer.data("timezone", time.strftime('%z'))
+
+        sentrypeer.data('source_address', line['source_ip']) if 'source_ip' in line else None
+        sentrypeer.data('target_address', line['destination_ip']) if 'destination_ip' in line else None
+        sentrypeer.data('source_port', '5060') 
+        sentrypeer.data('target_port', '5060') 
+        sentrypeer.data('source_protokoll', line['transport_type'].lower()) if 'transport_type' in line else None
+        sentrypeer.data('target_protokoll', line['transport_type'].lower()) if 'transport_type' in line else None
+
+        sentrypeer.request('description', 'Sentrypeer Honeypot')
+
+        sentrypeer.adata('hostname', ECFG['hostname'])
+        sentrypeer.adata('externalIP', ECFG['ip_ext'])
+        sentrypeer.adata('internalIP', ECFG['ip_int'])
+        sentrypeer.adata('uuid', ECFG['uuid'])
+
+        if sentrypeer.buildAlert() == "sendlimit":
+            break
+
+    sentrypeer.finAlert()
+    return()    
 
 
 """ --- [ MAIN ] ------------------------------------------------------------------ """
